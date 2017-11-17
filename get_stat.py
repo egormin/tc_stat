1f9c7322b38993794fffaa65cf4f3fe679da7d49
Your Name <you@example.com>
2017-11-17 12:22:18 -0800

    improved

--- get_stat.py
+++ get_stat.py
@@ -22,9 +22,36 @@ def builds():
     r = requests.get(url, headers=headers, auth=auth, timeout=10).json()
     return len(r['buildType'])
 
+prj = projects()
+builds = builds()
 table = BeautifulTable()
 table.column_headers = ["Name", "Count"]
-table.append_row(["Projects", projects()])
-table.append_row(["Build configurations", builds()])
+table.append_row(["Projects", prj])
+table.append_row(["Build configurations", builds])
 print(table)
+
+headder = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Report</title>
+</head>
+<body bgcolor="#d2691e">
+<table border="1">
+"""
+
+footer = """
+</table>
+</body>
+</html>"""
+
+content1 = "<tr><td>Projects counter: </td><td>{}</td></tr>".format(prj)
+content2 = "<tr><td>Projects counter: </td><td>{}</td></tr>".format(builds)
+content3 = table
+
+f = open('report/index.html', 'w')
+f.write(headder + content1 + content2 + content3 + footer)
+f.close()
+
 
