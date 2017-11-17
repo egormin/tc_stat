2ad5f1b86d4aacefed6a250a50259a35a4408798
Your Name <you@example.com>
2017-11-17 11:09:21 -0800

    x

--- get_stat.py
+++ get_stat.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
-#import requests
-import json
-url = "myUrl"
 
-print(url)
+import requests
+import json
+
+tc_url = "myUrl"
+
+auth = "myUser", "myPassword"
+
+headers = {'Accept': 'application/json'}
+
+url = tc_url + "/app/rest/buildTypes"
+r = requests.get(url, headers=headers, auth=auth, timeout=10)
+res = r.json()
+print(res)
 
 
