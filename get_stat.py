a3a72ce7c828555d56e16566063dc4d4bd492cd7
Your Name <you@example.com>
2017-11-17 12:33:10 -0800

    improved

--- get_stat.py
+++ get_stat.py
@@ -10,6 +10,7 @@ tc_url = "myUrl"
 auth = "myUser", "myPassword"
 headers = {'Accept': 'application/json'}
 
+from anytree import Node, RenderTree
 
 def projects():
     url = tc_url + "/app/rest/projects"
@@ -29,6 +30,49 @@ table.column_headers = ["Name", "Count"]
 table.append_row(["Projects", prj])
 table.append_row(["Build configurations", builds])
 print(table)
+
+
+first_lev = []
+sec_lev = []
+def tree():
+    url = tc_url + "/app/rest/projects"
+    r = requests.get(url, headers=headers, auth=auth, timeout=10)
+    res = r.json()
+
+    for i in range(1, len(res['project'])):
+        if res['project'][i]['parentProjectId'] == "_Root":
+            first_lev.append(res['project'][i]['id'])
+
+            # array L2
+    for i in range(0, len(first_lev)):
+        for x in range(0, len(res['project'])):
+            if res['project'][x]['id'] == first_lev[i]:
+                first_l = Node(res['project'][x]['name'])
+
+        sec_lev = []
+        for x in range(1, len(res['project'])):
+            if res['project'][x]['parentProjectId'] == first_lev[i]:
+                sec_lev.append(res['project'][x]['id'])
+
+        for j in range(0, len(sec_lev)):
+            for x in range(0, len(res['project'])):
+                if res['project'][x]['id'] == sec_lev[j]:
+                    second_l = Node(res['project'][x]['name'], parent=first_l)
+
+            for m in range(1, len(res['project'])):
+                if res['project'][m]['parentProjectId'] == sec_lev[j]:
+                    third_l = Node(res['project'][m]['name'], parent=second_l)
+
+                    # for pre, fill, node in RenderTree(first_l):
+                    #     print("%s%s" % (pre, node.name))
+
+        for pre, fill, node in RenderTree(first_l):
+            print("%s%s" % (pre, node.name))
+    return ""
+
+print(tree())
+
+
 
 headder = """
 <!DOCTYPE html>
